fef85eae084b982f771e7add6a28a25c0337a450
Reinalyn Ben <rein@Reinalyns-MacBook-Air.local>
2026-02-02 06:14:44 -0800

    add data

---
+++ src/lib/data-service/UserService/mocks/UserProgressData.ts
@@ -0,0 +1,7 @@
+import { currentLevelData } from './CurrentLevel'
+import { sampleWorkouts } from './WeeklyProgress'
+
+export const dataSource = {
+  currentLevels: currentLevelData,
+  weeklyProgress: sampleWorkouts,
+}
